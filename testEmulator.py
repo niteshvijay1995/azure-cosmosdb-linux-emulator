dc28b8d4a5178d4ec3bccdf9ac3c5e46922f4774
Nitesh Vijay <niteshvijay1995@gmail.com>
2024-05-29 17:51:36 +0530

    Update testEmulator.py

--- testEmulator.py
+++ testEmulator.py
@@ -7,9 +7,10 @@ endpoint = "https://127.0.0.1:8081/"
 key = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="
 database_name = "SampleDatabase"
 container_name = "SampleContainer"
+cert_path = "/etc/ssl/certs/emulatorcert.pem"
 
 # Initialize the Cosmos client
-client = CosmosClient(endpoint, key)
+client = CosmosClient(endpoint, key, ssl_verify=cert_path)
 
 try:
     # Create a database
