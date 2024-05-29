1c0fadd46c3ae7b92aa00d67dfddd54f390154b0
Nitesh Vijay <niteshvijay1995@gmail.com>
2024-05-29 18:58:51 +0530

    Update testEmulator.py

--- testEmulator.py
+++ testEmulator.py
@@ -3,7 +3,7 @@ import uuid
 from azure.cosmos import CosmosClient, PartitionKey, exceptions
 
 # Configure the connection to the Cosmos DB emulator
-endpoint = "https://127.0.0.1:8081/"
+endpoint = "https://localhost:8081/"
 key = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="
 database_name = "SampleDatabase"
 container_name = "SampleContainer"
